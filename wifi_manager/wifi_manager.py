fa75f4b6c8374a6e923c0cbe56c1b8f6af512d52
Martin Mastny <mastnym@vscht.cz>
2023-01-02 16:05:53 +0100

    webrepl disabled, config created if it doesnt exist, saving new wifi to config

--- wifi_manager/wifi_manager.py
+++ wifi_manager/wifi_manager.py
@@ -5,9 +5,6 @@ Config is loaded from a file kept by default in '/networks.json'
 Priority of networks is determined implicitly by order in array, first being the highest.
 It will go through the list of preferred networks, connecting to the ones it detects present.
 
-Default behaviour is to always start the webrepl after setup,
-and only start the access point if we can't connect to a known access point ourselves.
-
 Future scope is to use BSSID instead of SSID when micropython allows it,
 this would allow multiple access points with the same name, and we can select by signal strength.
 
@@ -20,10 +17,6 @@ import os
 
 # Micropython modules
 import network
-try:
-    import webrepl
-except ImportError:
-    pass
 try:
     import uasyncio as asyncio
 except ImportError:
@@ -41,9 +34,22 @@ except ImportError:
                             "critical": fake_log})()
 
 class WifiManager:
-    webrepl_triggered = False
     _ap_start_policy = "never"
     config_file = '/networks.json'
+    preferred_networks = []
+    ap_config = {}
+    default_config = {
+        "known_networks": [],
+        "access_point": {
+            "config": {
+                "essid": "Micropython",
+                "channel": 11,
+                "hidden": False,
+                "password": "P@55W0rd"
+            },
+            "start_policy": "fallback"
+        }
+    }
 
     # Starts the managing call as a co-op async activity
     @classmethod
@@ -64,7 +70,7 @@ class WifiManager:
                 # Ignore connecting status for now.. ESP32 is a bit strange
                 # if status != network.STAT_CONNECTING: <- do not care yet
                 cls.setup_network()
-            await asyncio.sleep(10)  # Pause 5 seconds
+            await asyncio.sleep(5)  # Pause 5 seconds
 
     @classmethod
     def wlan(cls):
@@ -88,17 +94,20 @@ class WifiManager:
         try:
             with open(cls.config_file, "r") as f:
                 config = json.loads(f.read())
-                cls.preferred_networks = config['known_networks']
-                cls.ap_config = config["access_point"]
-                if config.get("schema", 0) != 2:
-                    log.warning("Did not get expected schema [2] in JSON config.")
-        except Exception as e:
-            log.error("Failed to load config file, no known networks selected")
-            cls.preferred_networks = []
-            return
 
+        except OSError as e:
+            # config file not found
+            log.info("Creating config file")
+            with open(cls.config_file, "w") as f:
+                json.dump(cls.default_config, f)
+        except Exception as e:
+            log.error("Failed to load config file")
+            cls.preferred_networks = []
+            return False
+
+        cls.preferred_networks = config['known_networks']
+        cls.ap_config = config["access_point"]
         # set things up
-        cls.webrepl_triggered = False  # Until something wants it
         cls.wlan().active(True)
 
         # scan what’s available
@@ -119,8 +128,8 @@ class WifiManager:
                     connection_data = {
                         "ssid": aNetwork["ssid"],
                         "bssid": aNetwork["bssid"],  # NB: One day we might allow collection by exact BSSID
-                        "password": aPreference["password"],
-                        "enables_webrepl": aPreference["enables_webrepl"]}
+                        "password": aPreference["password"]
+                    }
                     candidates.append(connection_data)
 
         for new_connection in candidates:
@@ -129,7 +138,6 @@ class WifiManager:
             if cls.connect_to(ssid=new_connection["ssid"], password=new_connection["password"],
                               bssid=new_connection["bssid"]):
                 log.info("Successfully connected {0}".format(new_connection["ssid"]))
-                cls.webrepl_triggered = new_connection["enables_webrepl"]
                 break  # We are connected so don't try more
 
 
@@ -140,18 +148,9 @@ class WifiManager:
         if should_start_ap:  # Only bother setting the config if it WILL be active
             log.info("Enabling your access point...")
             cls.accesspoint().config(**cls.ap_config["config"])
-            cls.webrepl_triggered = cls.ap_config["enables_webrepl"]
         cls.accesspoint().active(cls.wants_accesspoint())  # It may be DEACTIVATED here
 
         # may need to reload the config if access points trigger it
-
-        # start the webrepl according to the rules
-        if cls.webrepl_triggered:
-            try:
-                webrepl.start()
-            except NameError:
-                # Log error here (not after import) to not log it if webrepl is not configured to start.
-                log.error("Failed to start webrepl, module is not available.")
 
         # return the success status, which is ultimately if we connected to managed and not ad hoc wifi.
         return cls.wlan().isconnected()
@@ -162,6 +161,25 @@ class WifiManager:
 
         for check in range(0, 10):  # Wait a maximum of 10 times (10 * 500ms = 5 seconds) for success
             if cls.wlan().isconnected():
+                log.info("SSID %s connected" % ssid)
                 return True
             time.sleep_ms(500)
         return False
+
+    @classmethod
+    def save_network(cls, ssid, password):
+        # try:
+            with open(cls.config_file, "r") as f:
+                config = json.load(f)
+            known = [network['ssid'] for network in config["known_networks"]]
+            if ssid in known:
+                log.warning('SSID already set up, overwriting...')
+                known = [network for network in config["known_networks"] if network['ssid'] != ssid]
+            new = {'ssid': ssid, 'password': password}
+            config["known_networks"] = [new] + known
+            log.info('Saving wifi network with SSID: %s' % ssid)
+            with open(cls.config_file, "w") as f:
+                json.dump(config, f)
+        # except Exception as e:
+        #     print(e)
+        #     log.error("Could not save new ssid")
